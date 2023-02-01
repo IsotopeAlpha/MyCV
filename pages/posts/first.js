593d9c5206ea9fac153254631fd43427c4600796
IIsotope <isotopegayina606@gmail.com>
2023-02-01 18:02:00 +0000

    done with integrating css

--- pages/posts/first.js
+++ pages/posts/first.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
-import Layout from "../../Layout/components/layout";
+import Layout from "../../Layout/components/layout.js";
 
 export default function FirstPost() {
     return (<Layout>
